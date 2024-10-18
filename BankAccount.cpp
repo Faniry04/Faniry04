9dfe8c0c142fdc5c237223d8a0ba1e43f3fefa20
Faniry04 <faniry.andriamasinoro1@gmail.com>
2024-10-18 15:05:23 +0200

    Création class BankAccount

---
+++ BankAccount.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Faniry Andriamasinoro on 18/10/2024.
+//
+
+#include "BankAccount.h"
---
+++ BankAccount.h
@@ -0,0 +1,15 @@
+//
+// Created by Faniry Andriamasinoro on 18/10/2024.
+//
+
+#ifndef BANQUE_BANKACCOUNT_H
+#define BANQUE_BANKACCOUNT_H
+
+
+class BankAccount {
+public :
+    bool retrait_argent(float somme)
+};
+
+
+#endif //BANQUE_BANKACCOUNT_H

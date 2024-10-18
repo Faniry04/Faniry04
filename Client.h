2dc81ee247a133c9dd11b9f2afc157bc7df8b296
Faniry04 <faniry.andriamasinoro1@gmail.com>
2024-10-18 14:41:14 +0200

    Création class Client

---
+++ Client.h
@@ -0,0 +1,20 @@
+//
+// Created by Faniry Andriamasinoro on 18/10/2024.
+//
+
+#ifndef BANQUE_CLIENT_H
+#define BANQUE_CLIENT_H
+#include <iostream>
+
+
+class Client {
+public :
+    char nom;
+    char prenom;
+private :
+    Date date_naissance;
+    char Id;
+};
+
+
+#endif //BANQUE_CLIENT_H
